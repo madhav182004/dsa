ee80be6e6b67ba1788fb1333299e1336c1b5cfa0
Madhav Mundhra <madhavmundhra221@gmail.com>
2025-04-18 16:59:19 +0530

    dynamic programming p12

---
+++ Dynamic Programming/12.GuessNumHighOrLow2.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// int solveUsingRecursion(int start, int end)
+// {
+//     if(start>=end)
+//     {
+//         return 0;
+//     }
+//     int ans = INT_MAX;
+//     for(int i=start; i<=end; i++)
+//     {
+//         ans = min(ans, i + max(solveUsingRecursion(start, i-1), solveUsingRecursion(i+1, end)));
+//     }
+//     return ans;
+// }
+// int solveUsingMemo(int start, int end, vector<vector<int>>& dp)
+// {
+//     if(start>=end)
+//     {
+//         return 0;
+//     }
+//     if(dp[start][end] != -1)
+//     {
+//         return dp[start][end];
+//     }
+//     int ans = INT_MAX;
+//     for(int i=start; i<=end; i++)
+//     {
+//         ans = min(ans, i + max(solveUsingMemo(start, i-1, dp), solveUsingMemo(i+1, end, dp)));
+//     }
+//     dp[start][end] = ans;
+//     return dp[start][end];
+// }
+int solveUsingTabulation(int n)
+{
+    vector<vector<int> >dp(n+2, vector<int>(n+1, 0));
+    for(int start_index = n-1;start_index>=1;start_index--)
+    {
+        for(int end_index = 1; end_index<=n;end_index++)
+        {
+            if(start_index >= end_index)
+            {
+                continue;
+            }
+            int ans = INT_MAX;
+            for(int i=start_index; i<=end_index; i++)
+            {
+                ans = min(ans, i + max(dp[start_index][i-1], dp[i+1][end_index]));
+            }
+            dp[start_index][end_index] = ans;
+        }
+    }
+    return dp[1][n];
+}
+int getMoneyAmount(int n) {
+    // int start = 0;
+    // int end = n;
+    // vector<vector<int> >dp(n+1, vector<int>(n+1, -1));
+    int ans = solveUsingTabulation(n);
+    return ans;
+}
+int main()
+{
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    int ans = getMoneyAmount(n);
+    cout<<"Output: "<<ans<<endl;
+}
