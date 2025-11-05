243bb1f318149daef6c623c2eec944188d5016cb
Madhav Mundhra <madhavmundhra221@gmail.com>
2025-11-05 23:41:36 +0530

    gfg potd nov5

---
+++ zGFG/ProblemOfTheDay/5nov.MinSquare.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int minSquares(int n)
+{
+    vector<int> dp(n+1, 1e9);
+    dp[0] = 0;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j*j<=i;j++)
+        {
+            dp[i] = min(dp[i], dp[i-j*j] + 1);
+        }
+    }
+    return dp[n];
+}
+int main() {
+    int n;
+    cin >> n;
+    cout << minSquares(n) << endl;
+    return 0;
+}
